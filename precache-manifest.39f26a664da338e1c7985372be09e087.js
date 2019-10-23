e6b6368e30545566956837935e3bc221497c2e79
lechcirmirakis <lech.cirmirakis@go2.pl>
2019-10-23 20:02:21 +0200

    Updates

---
+++ precache-manifest.39f26a664da338e1c7985372be09e087.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "460365c85133f0d23db94d38795c19e5",
+    "url": "/react_workers_list/index.html"
+  },
+  {
+    "revision": "7c01320c13556fabb51f",
+    "url": "/react_workers_list/static/css/main.a6ace492.chunk.css"
+  },
+  {
+    "revision": "1a77ad6bfafe011da3d8",
+    "url": "/react_workers_list/static/js/2.4ebac00b.chunk.js"
+  },
+  {
+    "revision": "7c01320c13556fabb51f",
+    "url": "/react_workers_list/static/js/main.e980411f.chunk.js"
+  },
+  {
+    "revision": "3f493633e8e6776666dc",
+    "url": "/react_workers_list/static/js/runtime-main.81634d44.js"
+  },
+  {
+    "revision": "86377fca9a2188d4d1eaafa1576cf3e1",
+    "url": "/react_workers_list/static/media/bg.86377fca.png"
+  },
+  {
+    "revision": "7b268d9868e8eba9d028a4769dd96e27",
+    "url": "/react_workers_list/static/media/bg_slider.7b268d98.png"
+  }
+]);
