04fedabd431305edfd68246ea81cb3b0f6aab8ba
lechcirmirakis <lech.cirmirakis@go2.pl>
2019-10-23 20:06:55 +0200

    fixed mail.png link

--- src/components/Members/Mail.js
+++ src/components/Members/Mail.js
@@ -3,7 +3,7 @@ import React from 'react';
 const mail = props => {
 	return (
 		<a className="mail" href={"mailto:" + props.mail}>
-			<img src="/images/mail.png" alt="" />
+			<img src="images/mail.png" alt="" />
 		</a>
 	)
 }
